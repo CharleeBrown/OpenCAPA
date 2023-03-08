b3d9c0b44cc00fb902a8a27817aef941eee1ffb6
Coleman <charleebrown22@gmail.com>
2023-03-08 08:41:36 -0600

    Commenting

--- OpenCAPA_Engine/DataLayer.cs
+++ OpenCAPA_Engine/DataLayer.cs
@@ -100,7 +100,63 @@ namespace OpenCAPA_Engine
 
         }
 
+        public void LoadCapaItem(string Title, string Description)
+        {
+            // Grabbing the connection string
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
 
+            // Initializing the CAPA ID return variable
+            int returnCAPA = 0;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                try
+                {
+                    // Creating the command, inserting into the table   
+                    SqlCommand comms = new SqlCommand();
+
+                    comms.Connection = conn;
+                    comms.CommandText = @"INSERT INTO CAPA_DATA VALUES (@Title, @Descrip)";
+                    comms.Parameters.AddWithValue("@Title", Title);
+                    comms.Parameters.AddWithValue("@Descrip", Description);
+
+                    comms.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.ToString());
+                }
+
+                try
+                {
+
+                    // While the connection is still open, execute this query.
+                    SqlCommand CommentCom = new SqlCommand();
+
+                    CommentCom.Connection = conn;
+                    CommentCom.CommandText = @"SELECT CAPA_ID FROM CAPA_DATA WHERE DESCRIP = @Descrip";
+                    CommentCom.Parameters.AddWithValue("@Descrip", Description);
+
+                    SqlDataReader reader = CommentCom.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int CAPA = Convert.ToInt32(reader["CAPA_ID"]);
+                        returnCAPA = CAPA;
+                    }
+                    AddComment(Description, returnCAPA);
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.Message);
+                }
+
+                conn.Close();
+
+            }
+
+
+        }
 
 
     }
